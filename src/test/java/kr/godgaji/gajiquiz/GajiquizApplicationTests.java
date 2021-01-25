89e48b4f29a41662307377c0ad55075935fb2ba3
riyenas0925 <riyenas0925@gmail.com>
2021-01-25 16:20:07 +0900

    feat : 스프링 부트 프로젝트 초기 설정 [#1]
    * 프로젝트 관련 dependency 추가
    
    Resolved #1

---
+++ src/test/java/kr/godgaji/gajiquiz/GajiquizApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.godgaji.gajiquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GajiquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
