6c5318c12bf31b1c56d217045e484d58000021b1
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-02-05 15:05:12 +0900

    chore : application-test.yml추가

--- src/test/java/kr/godgaji/gajiquiz/domain/RoleTest.java
+++ src/test/java/kr/godgaji/gajiquiz/domain/RoleTest.java
@@ -4,11 +4,11 @@ import kr.godgaji.gajiquiz.domain.Member.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yml" })
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RoleTest {
 
